bd32eb6f7dc898a09f3394aff96a9db43a5ca5b7
Tomas <tomascurto1@gmail.com>
2023-07-04 09:59:48 -0300

    agregue una linea en 34

--- ejercicios/01_entrada_salida/ejercicio_01/app_ES_01.py
+++ ejercicios/01_entrada_salida/ejercicio_01/app_ES_01.py
@@ -29,6 +29,7 @@ class App(customtkinter.CTk):
         self.btn_mostrar.grid(row=2, pady=20, columnspan=2, sticky="nsew")
 
     def btn_mostrar_on_click(self):
+        """Agregue una linea"""
         pass
 
 
